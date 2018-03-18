59745e1b3d6d9f65aafa2976b412237bf011a49e
Mozillex <Mozillex@users.noreply.github.com>
2018-03-18 00:16:35 -0400

    both api calls working (individual calls to users, and then combined call to get additional info.
    combined call not getting info as needed, so will rework.

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 
-
-var users = ["ESL_SC2","Monstercat","Viliciousness","freecodecamp","stefl1504","IMP_meles"];//,"jessicamak","malukah","DizzyKitten","legendarylea","moushn"];
+var users = ["ESL_SC2","Monstercat","Viliciousness","freecodecamp","stefl1504","IMP_meles","jessicamak","malukah","DizzyKitten","legendarylea","moushn"];
 var streamArr = [];
-
 
 function twitch(users){
 	for (var each in users){
-		//let uri = 'https://api.twitch.tv/kraken/streams' + '?' + users[each];
 		let uri = 'https://api.twitch.tv/helix/users' + '?login=' + users[each];
 		getUser(uri);
 	}
@@ -66,32 +63,34 @@ function showCard(user){
 }
 
 function getStreams(){
-		console.log(streamArr);
-//	console.log(streamArr.join('ff'));
 
-	// let uri = 'https://api.twitch.tv/helix/streams' + '?user_id='+ streamArr.join('&user_id=');
-	// let request = new XMLHttpRequest();
-	// request.open('get',uri,true);
-	// request.dataType = 'json';
-	// request.responseType = 'json';
-	// request.setRequestHeader('Client-ID','kspag9hdl5zkpy6j9oczwkg9t4n6fm');
-	// request.onerror = function(){
-	// 	console.log("oops. stream request failed");
-	// };
-	//
-	// request.onload = function(o){
-	// 	if (o.hasOwnProperty('error')){
-	// 		console.log('onload error: ' + o.error + o.message);
-	// 		return;
-	// 	};
-	//
-	// 	console.log("streams onload succesful. finish the onload function");
-	// 	let response2 = this.response;
-	// 	//let data = response2.data[0];
-	// 	console.log(response2);
-	// }
-	//
-	// request.send();
-}
+	users = users.join('&login=');
+	// var ids = streamArr.join('&user_id='); //would need to set up a delay to use this
+
+	var uri1 = 'https://api.twitch.tv/helix/streams' + '?login='+ users;
+	// var uri2 = 'https://api.twitch.tv/helix/videos' + '?user_id' + ids; //again - delay required
+
+	let request = new XMLHttpRequest();
+
+	request.open('get',uri1,true);
+	request.dataType = 'json';
+	request.responseType = 'json';
+	request.setRequestHeader('Client-ID','kspag9hdl5zkpy6j9oczwkg9t4n6fm');
+	request.onerror = function(){
+		console.log("oops. stream request failed");
+	};
+
+	request.onload = function(){
+		let response = this.response;
+		let data = response.data[0];
+		response.data.map(e => {
+			console.log(e.title);
+		})
+
+	};
+
+
+	request.send();
+	}
 
 twitch(users);
