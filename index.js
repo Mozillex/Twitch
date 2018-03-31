2a012629956fa33f2950f860deda74e735e1fa39
Mozillex <Mozillex@users.noreply.github.com>
2018-03-31 01:22:19 -0400

    working version as posted 3/31/2018 to codepen. https://codepen.io/Mozillex/pen/rddgoe
    still needing to update channel/stream details on the 'back side' of the
    player cards. placeholder text in its place at this commit.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var users = ["ESL_SC2","Monstercat","Viliciousness","freecodecamp","legendarylea","stefl1504","malukah","DizzyKitten","IMP_meles"];
+var apiData1 = {};
+var apiData2 = {};
 
 function get(id){
    return document.getElementById(id);
@@ -8,16 +10,15 @@ function newEl(el){
    return document.createElement(el);
 }
 
-var content= get('content'), flipped = false;
+var content = get('content');
+var opened = 0;
 
-content.innerHTML = "";
-
-function callTwitch(){//arguments: 1st-> users([]); 2nd-> get type('')
+function callTwitch(){
 
    let args = [...arguments];
    let players = args[0];
    var endType = args[1];
-   let login = args[2];
+   let login = (args[2] ? args[2] : '');
    let params = args[1] + (endType==='users'?'?login=':'?user_id=');
    let multiPlayers = (players.length>1?true:false);
    let concat = (endType==='users'?'&login=':'&user_id=');
@@ -33,185 +34,211 @@ function callTwitch(){//arguments: 1st-> users([]); 2nd-> get type('')
 	};
 
 	request.onload = function(){
-		if (request.status !== 200){
-         return console.log('Twitch Fix!! status code = '+request.status+': '+request.statusText);
-		}
+
+		if (request.status !== 200) return alert('API Request error\n'+request.status+': '+request.statusText);
 
       let response = this.response;
 
-      if (response.error) return console.log(response.error +': '+ response.status);
+      if (response.error) return alert('Network Error\n'+response.error +': '+response.status+"\nPlease try again.");
 
-      let group = response.data;
 
-      if (group.length > 1){
-         for (each in group) {
-            makeCard(group[each]);
+      else {
+
+         let group = response.data;
+
+         if (group.length > 1){
+            content.innerHTML = "";
+            for (each in group) {
+               makeCard(group[each]);
+            };
          }
+
+         else updateFront([players,response,login]);
       }
-
-      else updateFront([players,response,login]);
-
 	};
 
 	request.send();
 }
 
+
 function makeCard(user){
 
-	let name = (user.display_name.length > 2 ? user.display_name : user.login);
+   let name = (user.display_name.length > 2 ? user.display_name : user.login);
 	let id = user.id;
-   let wrp_id = 'wrp_'+id;
    let login = user.login;
 	let description = user.description;
 	let profPic = user.profile_image_url;
 
    let head = newEl('div');//container for front and back
-   head.setAttribute('id', wrp_id);
+   head.setAttribute('id', 'H'+id);
 
-	let card = newEl('div');//front of card
-	card.setAttribute('id', id);
+	let front = newEl('div');
+	front.setAttribute('id', 'F'+id);
+
    let picDiv = newEl('div');
    picDiv.setAttribute('class','img');
+
    let profImg = newEl('img');
    profImg.src = profPic;
+
    let h1 = newEl('h1');
    h1.innerText = name;
+
    let p = newEl('p');
    p.innerText = description;
 
-   let content = get('content');
    content.appendChild(head);
-   head.appendChild(card);
-   card.appendChild(picDiv);
+   head.appendChild(front);
+   front.appendChild(picDiv);
    picDiv.appendChild(profImg);
-   card.appendChild(h1);
-   card.appendChild(p);
+   front.appendChild(h1);
+   front.appendChild(p);
 
    callTwitch([id],'streams',login);
 }
-var live;
+
 
 function updateFront(args) {
-
-   let id = args[0][0];
+   let id = args[0][0];//this is the same as user id
    let response = args[1].data;
+
    let login = args[2];
-   live = (response[0]?true:false);
+
+   let live = (response[0]? true : false);
    let status = (live? response[0].type :'offline');
 
-	let card = get(id);
-	card.setAttribute('class', status);
+   if (live) console.log(id+', '+response+', '+login);
+
+	let front = get('F' + id);
+	front.className = status;
 
 	let indicator = newEl('div');
-	indicator.setAttribute('class', 'controls status');
+	indicator.className = 'controls status';
+
    let span = newEl('span');
-   span.setAttribute('class', status);
+   span.className = status;
    span.textContent = status;
 
    let link = newEl('div');
-   link.setAttribute('class', 'controls link');
+   link.className = 'controls link';
 
    let btn = newEl('div');
-   btn.setAttribute('class', 'button');
    btn.id = 'btn'+id;
-   btn.name = login;
-   let tail = login + '&autoplay=false';
-   btn.setAttribute('href',live? "http://player.twitch.tv?" + tail : "https://www.twitch.tv/" + tail);
+   btn.className = 'button';
 
-   btn.innerText = (live? "view feed": "view chanel");
+   btn.flipped = false;
+   btn.innerText = (live? "view feed": "view channel");
+   btn.name = login;
+   btn.feed = live;
+   btn.codeNum = id;
+
+   btn.href = "https://player.twitch.tv?channel=" + login + "&autoplay=false";
+
+   btn.target = '#iF'+id;
+
    btn.onclick = flipCard;
 
-   link.appendChild(btn);//(i)
+   link.appendChild(btn);
 
-   card.appendChild(indicator);
+   front.appendChild(indicator);
    indicator.appendChild(span);
-	card.appendChild(link);
+	front.appendChild(link);
 
-   makeBack(login,id);
-
-
+   makeBack(login,id,live,status);
 }
 
-function makeBack(login,id){
 
-   console.log(status);
+function makeBack(login,id,live,status){
 
-   sideB = newEl('div');
-   sideB.setAttribute('id', 'B'+id);
-   sideB.style.display = 'none';
+   let head = get('H'+id);
 
+   let back = newEl('div');
+   back.id = 'B'+id;
+   back.style.display = 'none';
 
    let divFrame = newEl('div');
-   let iFrame = newEl('iframe');
-   iFrame.setAttribute('id','iF'+id);
-   iFrame.setAttribute('width','400');
-   iFrame.setAttribute('height','300');
-   iFrame.setAttribute('frameborder','0');
-   iFrame.setAttribute('scrolling','no');
-   iFrame.setAttribute('allowfullscreen','false');
+
+   let frame = newEl('iframe');
+   frame.id = 'iF' + id;
+   frame.name = 'iF' + id;
+   frame.setAttribute('width','400');
+   frame.setAttribute('height','300');
+   frame.setAttribute('frameborder','0');
+   frame.setAttribute('scrolling','no');
+   frame.setAttribute('allowfullscreen','false');
 
    let h1 = newEl('h1')
    h1.innerText = 'update.title';
+
    let p1 = newEl('p');
    p1.textContent = 'Game ID: '+'update.game.id';
+
    let p2 = newEl('p2');
    p2.textContent = 'Viewer Count: '+ 'update.viewer_count';
 
    let indicator = newEl('div');
-   indicator.setAttribute('class', 'controls status');
+   indicator.className = 'controls status';
+
    let span = newEl('span');
-   span.setAttribute('class', status);
+   span.className = status;
    span.textContent = live? "LIVE": "OFFLINE";
 
    let link = newEl('div');
-   link.setAttribute('class', 'controls link');
+   link.className = 'controls link';
 
    let rtnBtn = newEl('div');
-   rtnBtn.setAttribute('class', 'button');
-   rtnBtn.setAttribute('id','rtn_btn'+ id);
-   rtnBtn.setAttribute('name','return'+login);
-   rtnBtn.innerText = 'return';
+   rtnBtn.className = 'button';
+   rtnBtn.id = 'rtn_btn'+ id;
+   rtnBtn.name = login;
+   rtnBtn.innerText = 'close';
+   rtnBtn.codeNum = id;
+   rtnBtn.flipped = true;
    rtnBtn.onclick = flipCard;
 
-   let head = 'wrp_'+id;
-   get(head).appendChild(sideB);
-   sideB.appendChild(divFrame);
-   divFrame.appendChild(iFrame);
-   sideB.appendChild(h1);
-   sideB.appendChild(p1);
-   sideB.appendChild(p2);
-   sideB.appendChild(indicator);
+   head.appendChild(back);
+   back.appendChild(divFrame);
+   divFrame.appendChild(frame);
+
+   back.appendChild(h1);
+   back.appendChild(p1);
+   back.appendChild(p2);
+
+   back.appendChild(indicator);
    indicator.appendChild(span);
-   sideB.appendChild(link);
+
+   back.appendChild(link);
    link.appendChild(rtnBtn);
 }
 
+
 function flipCard(e){
 
-   let login = this.name;
+   var id = this.codeNum;
+   var login = this.name;
 
-   if (!flipped){
-      let id = this.id.slice(3);
-      let id_b = 'B'+id;
-      let id_iF = 'iF'+id;
-      get(id).style.display = 'none';
-      get(id_b).style.display = 'block';
-      let tail = login +'&autoplay=false';
-      let uri = live? 'http://player.twitch.tv?channel=' + tail : "https://www.twitch.tv/" + tail;
-      get(id_iF).src = uri;//'http://player.twitch.tv?channel=' + login +'&autoplay=false';
-      flipped = true;
+   var front = get('F'+id);
+   var back = get('B'+id);
+   var frame = get('iF'+id);
+
+   if (this.flipped===false){
+
+      frame.src = this.href;
+      //window.open(uri,frame.name);
+      front.style.display = 'none';
+      back.style.display = 'block';
+      opened +=1;
    }
+
    else {
-      let id = this.id.slice(7);
-      let id_b = 'B'+id;
-      get(id).style.display = 'block';
-      get(id_b).style.display = 'none';
-      flipped=false;
+      front.style.display = 'block';
+      back.style.display = 'none';
+      frame.src = "";
+      opened -= 1;
    }
 
+   if (opened > 5) alert("y. u. so twitchy?!\nyou really tryin' 2 watch\nall these streams?");
 }
 
-/*		*** 	setup functions and listeners	***	*/
 
 function init(){
 	var input = get('input-1');
@@ -224,11 +251,7 @@ function init(){
 	input.value="";
 	callTwitch(users,'users');
 }
-callTwitch(users,'users','');
 
 get('btn-1').addEventListener('click', init);
 
-get('mod_btn').addEventListener('click',close);
-function close(){
-   get('modal').style.display = 'none';
-}
+callTwitch(users,'users');
